e3115bcbb3ca9f1b1f8c7ad6a62688adb8d4aae0
koustubh <koustubh28@gmail.com>
2022-02-05 18:33:20 +0530

    Commit 1: Base commit.

---
+++ Spring-LDAP-example/src/main/java/com/example/Spring/LDAP/example/SpringLdapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.LDAP.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLdapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLdapExampleApplication.class, args);
+	}
+
+}
